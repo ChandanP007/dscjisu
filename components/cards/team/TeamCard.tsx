ebff71a281313eb1ef3dd400c3d87febf85b8b0d
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-11-10 23:48:55 +0530

    build: update TeamCard.tsx

--- components/cards/team/TeamCard.tsx
+++ components/cards/team/TeamCard.tsx
@@ -17,14 +17,14 @@ export interface ITeamCard {
 const TeamCard: React.FC<ITeamCard> = ({ github, id, image, name, linkedin, role, tagline, twitter }) => {
   return (
     <>
-      <div className=' mx-5 p-0 flex flex-col max-w-xs  dark:bg-black m-3 bg- shadow-2xl  dark:shadow-grey-400  dark:shadow-lg   justify-center rounded-xl dark:border-white  border-2 dark:hover:bg-black dark:hover:shadow-lg dark:hover:shadow-teal-300 transition-all duration-200 ease-in  hover:scale-105  '
+      <div className=' mx-5 p-0 flex flex-col max-w-xs  bg-black m-3 bg-shadow-2xl  dark:shadow-grey-400  shadow-lg   justify-center rounded-xl border-white  border-2 hover:bg-black hover:shadow-lg hover:shadow-teal-300 transition-all duration-200 ease-in  hover:scale-105  '
         key={id} title={
           name + " " + role + " " + tagline
         } >
         <div className='flex justify-center pt-5 pb-1  rounded-lg'>
           <Avatar src={image} alt={name} sx={{ width: 150, height: 150 }} className={`z-[0] shadow-lg border-4 border-cyan-200`} />
         </div>
-        <div className="p-5 font-mono dark:text-white">
+        <div className="p-5 font-mono text-white">
           <h1 className='text-2xl text-center font-mono'>{name}</h1>
           <h3 className='text-center underline underline-offset-4 decoration-yellow-800 decoration-underline' title={role}>{role}</h3>
           <h2 className='text-center  font-thin font-sans'>{tagline}</h2>
