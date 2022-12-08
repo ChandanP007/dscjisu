e5ae748e56a9aaec7a82cb72e585cd449ddb06d9
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-12-08 09:10:36 +0530

    fix: type connectdb

--- lib/ConnectDb.ts
+++ lib/ConnectDb.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
+const connectMongo = async () => mongoose.connect(process.env.MONGO_URI!).then(() => console.log('MongoDB connected...'))
+    .catch((err) => console.log(err)
+    );
 
 export default connectMongo;
