af6b20b750ae731d946bbe65f7d48cdd97d274b8
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-12-14 11:38:34 +0530

    refactor: connect

--- layout/Connect.tsx
+++ layout/Connect.tsx
@@ -1,4 +1,48 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
+
+interface ISocial {
+    name: string;
+    link: string;
+    icon: string;
+}
+
+
+const SocialMediaCard = ({ icon, link, name }: ISocial) => {
+    return (
+        <Link href={link} passHref className="lg:basis-0 sm:basis-1/3 w-full">
+            <div className="flex justify-around gap-2 w-1/3  bg-white hover:bg-violet-700 p-2 rounded-lg  items-center text-center">
+                <Image width={35} height={35} className="button-icon" src={icon} loading="lazy" alt={name} />
+                <h1>{name}</h1>
+            </div>
+        </Link>
+    )
+
+}
+
+const SocialMedia = [
+    {
+        name: "GitHub",
+        link: "https://github.com/dscjisu",
+        icon: "/github-192x192.png"
+    },
+    {
+        name: "Linkedin®",
+        link: "https://www.linkedin.com/company/dscjisu",
+        icon: "/linkedin-192x192.png"
+    },
+    {
+        name: "Twitter",
+        link: "https://twitter.com/dscjisu",
+        icon: "/twitter-192x192.png"
+    },
+    {
+        name: "Facebook",
+        link: "https://www.facebook.com/groups/gdscjisu",
+        icon: "/facebook-192x192.png"
+    }
+
+]
 
 export default function Connect() {
     return (
@@ -10,20 +54,14 @@ export default function Connect() {
                             <h1 className="lg:text-6xl text-4xl font-mono text-white">Stay in touch</h1>
                             <p className="text-white lg:text-lg">We are always open to new ideas and suggestions. If you have any questions or want to get in touch with us, feel free to reach out to us.</p>
                             <div className="">
-                                <div className="p-10 flex lg:flex-row flex-col gap-4 justify-center text-center items-center">
-                                    <a href="https://github.com/dscjisu" className="w-full flex gap-2 bg-white hover:bg-violet-700 p-2 rounded-lg justify-center items-center text-center">
-                                        <Image width={32} height={30} className="button-icon" src="/github-192x192.png" loading="lazy" alt="" />GitHub
-                                    </a>
-                                    <a href="https://www.linkedin.com/company/dscjisu" className="w-full  flex gap-2 bg-white p-2 rounded-lg justify-center items-center text-center">
-                                        <Image width={32} height={30} className="button-icon" src="/linkedin-192x192.png" loading="lazy" alt="" />Linkedin®
-                                    </a>
-                                    <a href="https://twitter.com/dscjisu" className=" w-full flex gap-2 bg-white p-2 rounded-lg justify-center items-center text-center">
-                                        <Image width={32} height={30} className="button-icon" src="/twitter-192x192.png" loading="lazy" alt="" />Twitter
-                                    </a>
-                                    <a href="https://www.facebook.com/groups/gdscjisu" className=" w-full  flex gap-2 bg-white p-2 rounded-lg justify-center items-center text-center">
-                                        <Image width={32} height={30} className="button-icon" src="/facebook-192x192.png" loading="lazy" alt="" />Facebook
-                                    </a>
-
+                                <div className="p-10 flex lg:flex-row sm:flex-col gap-2 justify-center text-center items-center">
+                                    {
+                                        SocialMedia.map((item, index) => {
+                                            return (
+                                                <SocialMediaCard key={index} name={item.name} link={item.link} icon={item.icon} />
+                                            )
+                                        }
+                                        )}
                                 </div>
                             </div>
                         </div>
