54c49b2fdc78307f437928fcb7c57b9ee8a82de7
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-02-03 10:48:04 +0530

    tags

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -46,6 +46,23 @@ export default function Blog({ teams }: { teams: TeamMeta[] }) {
         <>
             <Head>
                 <title>Teams | Google Developer Student Clubs JIS University - Kolkata</title>
+                <meta name="description" content="We thank all the core team members for their contribution to the project, Events and the community. Without them, this community would not have been possible." />
+                <meta name="tags" content="team coreteam core team gdsc team gdsc india team" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:creator" content="@dscjisu" />
+                <meta name="twitter:title" content="Google Developer Student Clubs" />
+                <meta name="twitter:description" content="Connect - Learn - Grow" />
+                <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1556549974783303680/qPBMcmcN_400x400.jpg" />
+                <meta property="og:site_name" content="Google Developer Student Clubs JIS University Kolkata" />
+                <meta property="og:image" content="https://pbs.twimg.com/profile_images/1556549974783303680/qPBMcmcN_400x400.jpg" />
+                <meta property="og:image:secure_url" content="https://pbs.twimg.com/profile_images/1556549974783303680/qPBMcmcN_400x400.jpg" />
+                <meta property="og:image:width" content="1080" />
+                <meta property="og:image:height" content="1080" />
+                <meta property="og:title" content="JIS University - Kolkata | Google Developer Student Clubs" />
+                <meta property="og:description" content="Connect - Learn - Grow" />
+                <meta property="og:url" content="https://gdsc.community.dev/jis-university-kolkata/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="en_US" />
             </Head>
             <div className='lg:p-20 sm:p-2 bg-white flex  flex-col  dark:bg-[#182232] py-10'>
                 <div className="text-center">
